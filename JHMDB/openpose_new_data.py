3e7b2125223b678af8bbcca43e44a430e331e2cf
Ubuntu <ubuntu@cloudlet030.maas>
2020-04-23 17:47:35 +0000

    preprocess ava

--- JHMDB/openpose_new_data.py
+++ JHMDB/openpose_new_data.py
@@ -2,10 +2,12 @@ import json
 import glob
 import numpy as np
 import pickle
+import re
+import os
+import glob
 
 
-ACTIONS = ['clap1', 'clap2', 'wave1', 'wave2']
-LABELS = ['clap', 'clap', 'wave', 'wave']
+PATH_TO_VIDEOS = '/home/ubuntu/ava_openpose'
 
 
 def main():
@@ -14,10 +16,15 @@ def main():
         'label': []
     }
 
+    pattern = re.compile(r'^([a-z]+)\d+')
 
-    for action, label in zip(ACTIONS, LABELS):
-        openpose_path = '/home/ubuntu/roger_actions_output/' + action
-        openpose_file_paths = sorted(glob.glob(openpose_path + '/*'))
+    for video in os.listdir(PATH_TO_VIDEOS):
+
+        match = pattern.match(video)
+        label = match.group(1)
+
+        path_to_json_files = os.path.join(PATH_TO_VIDEOS, video, '*')
+        openpose_file_paths = sorted(glob.glob(path_to_json_files))
         
         all_points = pose_from_openpose(openpose_file_paths)
         pose = np.array(all_points)
@@ -25,7 +32,7 @@ def main():
         output['label'].append(label)
         output['pose'].append(pose)
 
-    pickle.dump(output, open('/home/ubuntu/roger_actions_output/pose_label.pkl', 'wb'))
+    pickle.dump(output, open('/home/ubuntu/ava_pose_label.pkl', 'wb'))
 
 
 KEYPOINT_INDICES = [1, 2, 5, 9, 12, 3, 6, 10, 13, 4, 7, 11, 14]
@@ -39,6 +46,8 @@ def pose_from_openpose(file_paths):
             content = json.load(json_file)
             people = content['people']
 
+            if len(people) != 1:
+                continue
             assert len(people) == 1
 
             keypoints = people[0]['pose_keypoints_2d']
